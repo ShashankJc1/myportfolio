08455ac177a89b35c57286cf524b4312b1957344
Shashank Janagam Chandra <sjanagam@stevens.edu>
2024-08-22 16:59:26 -0400

    bugFixes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,157 +1,186 @@
-"use client"
-import { animate, motion } from "framer-motion"
-import React, {useState} from "react"
-import {Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
-import {BsArrowUpRight, BsGithub} from "react-icons/bs"
-import {Tooltip, TooltipContent, TooltipTrigger, TooltipProvider} from "@/components/ui/tooltip"
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
 
-import Link from "next/link"
-import Image from "next/image"
-import WorkSliderBtns from "@/components/ui/WorkSliderBtns"
+import Link from "next/link";
+import Image from "next/image";
+import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
 
 const projects = [
-
   {
-    num: '01',
-    category: 'YouTube Video Summarizer',
-    title:'Project 1',
-    description: "Developed a YouTube video summarizer powered by Gemini, enabling users to generate concise summaries from video links instantly. Streamlines content consumption by delivering key insights in just a few clicks.",
-    stack: [{name: "Gemini"}, {name: "YouTube API"}, {name: "Python (NLP)"}],
-    image: '/assets/p2.webp',
+    num: "01",
+    category: "YouTube Video Summarizer",
+    title: "Project 1",
+    description:
+      "Developed a YouTube video summarizer powered by Gemini, enabling users to generate concise summaries from video links instantly. Streamlines content consumption by delivering key insights in just a few clicks.",
+    stack: [{ name: "Gemini" }, { name: "YouTube API" }, { name: "Python (NLP)" }],
+    image: "/assets/p2.webp",
     live: "https://youtube-summarizer-1.onrender.com/",
     github: "https://github.com/ShashankJc1/youtube-summarizer",
   },
   {
-    num: '02',
-    category: 'AI/ML',
-    title:'Project 2',
-    description: "Developed a personalized AI model to generate realistic images of myself based on text prompts, showcasing the versatility of AI in creative and professional applications. This project involved training the model with customized datasets, enabling high-quality image synthesis for various digital art and branding purposes. Actual link to model is private since anyone can create my images",
-    stack: [{name: "Python"}, {name:"CUDA"}, {name:"ComfyUI"}],
-    image: '/assets/a.jpg',
+    num: "02",
+    category: "AI/ML",
+    title: "Project 2",
+    description:
+      "Developed a personalized AI model to generate realistic images of myself based on text prompts, showcasing the versatility of AI in creative and professional applications. This project involved training the model with customized datasets, enabling high-quality image synthesis for various digital art and branding purposes. Actual link to model is private since anyone can create my images",
+    stack: [{ name: "Python" }, { name: "CUDA" }, { name: "ComfyUI" }],
+    image: "/assets/a.jpg",
     live: "https://huggingface.co/jcshashank/mrstorme-lora/tree/main",
     github: "https://github.com/ShashankJc1/image_generator",
   },
   {
-    num: '03',
-    category: 'Books Recommendation system',
-    title:'Project 3',
-    description: "The Books Recommendation Web App is a platform that uses machine learning algorithms like KNN and SVD to recommend the top 10 books based on ratings and cosine similarity. It features a clean and user-friendly interface built with HTML, CSS, Bootstrap, and JavaScript, and utilizes Express.js and Node.js for the backend, with MongoDB as the database. The system leverages a cleaned Goodreads dataset to provide personalized book recommendations.",
-    stack: [{name: "Html"}, {name: "css"}, {name: "Javascript"}, {name: "Node.js"}, {name: "MongoDB"}, {name: "KNN and SVD (ML)"}],
-    image: '/assets/img.webp',
-    live:"",
+    num: "03",
+    category: "Books Recommendation system",
+    title: "Project 3",
+    description:
+      "The Books Recommendation Web App is a platform that uses machine learning algorithms like KNN and SVD to recommend the top 10 books based on ratings and cosine similarity. It features a clean and user-friendly interface built with HTML, CSS, Bootstrap, and JavaScript, and utilizes Express.js and Node.js for the backend, with MongoDB as the database. The system leverages a cleaned Goodreads dataset to provide personalized book recommendations.",
+    stack: [
+      { name: "Html" },
+      { name: "css" },
+      { name: "Javascript" },
+      { name: "Node.js" },
+      { name: "MongoDB" },
+      { name: "KNN and SVD (ML)" },
+    ],
+    image: "/assets/img.webp",
+    live: "",
     github: "https://github.com/ShashankJc1/book-recommendation-system",
   },
-]
+];
 
 const Work = () => {
-  const [project, setProject] = useState(projects[0])
-  
+  const [project, setProject] = useState(projects[0]);
+
   const handleSlideChange = (swiper) => {
-    const currentIndex = swiper.activeIndex
-    setProject(projects[currentIndex])
-  } 
-  
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
   return (
-  <motion.section 
-    initial={{opacity: 0}} 
-    animate={{
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
         opacity: 1,
-        transition: {delay:2.4, duration: 0.4, ease: "easeIn"},
-      }} 
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
-    <div className="container mx-auto">
-      <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-        <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-          <div className="flex flex-col gap-[30px] h-[50%]">
-            <div className="text-8xl leading-none font-extrabold text-transparent text-outline text-white ">
-              {project.num}
-            </div>
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent
-                  transition-all duration-500 capitalize">
-                  {project.category} project
-                </h2>
-                <p className="text-white/60">{project.description}</p>
-                <ul className="flex gap-4">
-                  {project.stack.map((item, index) => {
-                    return (
-                      <li key={index} className="text-xl text-accent">
-                        {item.name}
-                        {index !== project.stack.length -1 && ","}
-                      </li>
-                    )
-                  })}
-                  
-                </ul>
-                <div className="border border-white/20"></div>
-                <div className="flex items-center gap-4">
-                  <Link href={project.live} target='_blank'>
-                    <TooltipProvider delayDuration={100}>
-                        <Tooltip>
-                          <TooltipTrigger className="w-[70px] h-[70px] rounded-full
-                            bg-white/5 flex justify-center items-center group">
-                              <BsArrowUpRight className="text-white text-3xl 
-                                group-hover:text-accent" />
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            <p>Live project</p>
-                          </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                  </Link>
-                  <Link href={project.github} target='_blank' >
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline text-white ">
+                {project.num}
+              </div>
+              <h2
+                className="text-[42px] font-bold leading-none text-white group-hover:text-accent
+                  transition-all duration-500 capitalize"
+              >
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4">
+                {project.live && (
+                  <Link href={project.live} target="_blank">
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
-                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full
-                          bg-white/5 flex justify-center items-center group">
-                            <BsGithub className="text-white text-3xl 
-                              group-hover:text-accent" />
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            <p>github Repository</p>
-                          </TooltipContent>
+                        <TooltipTrigger
+                          className="w-[70px] h-[70px] rounded-full
+                            bg-white/5 flex justify-center items-center group"
+                        >
+                          <BsArrowUpRight
+                            className="text-white text-3xl 
+                                group-hover:text-accent"
+                          />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
                       </Tooltip>
                     </TooltipProvider>
                   </Link>
-                </div>
+                )}
+                <Link href={project.github} target="_blank">
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger
+                        className="w-[70px] h-[70px] rounded-full
+                          bg-white/5 flex justify-center items-center group"
+                      >
+                        <BsGithub
+                          className="text-white text-3xl 
+                              group-hover:text-accent"
+                        />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>GitHub Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="w-full xl:w-[50%]">
-          <Swiper 
-            spaceBetween={30} 
-            SlidesPerView={1} 
-            className="xl:h-[520px] mb-12"
-            onSlideChange={handleSlideChange}>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
               {projects.map((project, index) => {
-                  return (
-                    <SwiperSlide key={index} className="w-full">
-                      <div className="h-[460px] relative group flex justify-center
-                        items-center bg-pink-50/20">
-                        <div className="absolute top-0 bottom-0 w-full bg-black/10 z-10"></div>
-                          <div className="relative w-full h-full">
-                            <Image
-                              src={project.image}
-                              fill
-                              className="object-cover"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                    </SwiperSlide>
-                )
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div
+                      className="h-[460px] relative group flex justify-center
+                        items-center bg-pink-50/20"
+                    >
+                      <div className="absolute top-0 bottom-0 w-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
               })}
-              <WorkSliderBtns 
+              <WorkSliderBtns
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] 
                 xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px]
                 w-[44px] h-[44px] flex justify-center items-center transition-all"
               />
-          </Swiper>
+            </Swiper>
+          </div>
         </div>
       </div>
-    </div>
-  </motion.section>
-  )
-}
+    </motion.section>
+  );
+};
 
-export default Work
+export default Work;
